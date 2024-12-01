cde488778a9469149e6bb7b8d8913f1563e7c524
XinWu007 <zyj1184204728@outlook.com>
2024-12-01 15:47:21 +0800

    refined Contact class: added specified comments and instructions

--- src/Contact.java
+++ src/Contact.java
@@ -1,10 +1,28 @@
+/*
+#1 constructor
+#2 private non-static cc helper
+#3 private static pn2s1 helper
+#4 public non-static getter
+#5 public non-static to setter
+#6 public static welcome
+#7 public static cnt getter
+#8 private static pn2s2 helper
+ */
+
 public class Contact {
     public String name;
     public int[] phoneNumber;
-    private int confirmCode;
+    private int confirmationCode;
     public static int counter = 0;
 
-    private int calculateConfirmCode() {
+    /* #2
+     * This function calculates the confirmation code
+     * by adding the first character of the name
+     * and the sum of the phone numbers, and mod the total by 11.
+     *
+     * accessible within this class only
+     */
+    private int calculateConfirmationCode() {
         int sum = name.charAt(0);
         for (int number : phoneNumber) {
             sum += number;
@@ -12,27 +30,21 @@ public class Contact {
         return sum % 11;
     }
 
-    public Contact(String name, int[] phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.confirmCode = calculateConfirmCode();
-        counter ++;
+    /* #3
+     * private helper function to convert an array of integers into a string
+     */
+    private String phoneNumberToString1() {
+        String res = "";
+        for (int num : phoneNumber) {
+            res += num;
+        }
+        return res;
     }
 
-    public String getContactInfo() {
-        return "The following are the information of the current contact: \n" +
-                "Name: " + name +
-                ",\nPhone Number: " + phoneNumberToString(phoneNumber) +
-                ",\nConfirm Code: " + confirmCode + ";";
-    }
-
-    public void setContactInfo(String name, int[] phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.confirmCode = calculateConfirmCode();
-    }
-
-    private static String phoneNumberToString(int[] arr) {
+    /* #6
+     * private helper function to convert an array of integers into a string
+     */
+    private static String phoneNumberToString2(int[] arr) {
         String res = "";
         for (int num : arr) {
             res += num;
@@ -40,10 +52,60 @@ public class Contact {
         return res;
     }
 
+    /* #1
+     * the constructor
+     *
+     * accessible in other class
+     */
+    public Contact(String name, int[] phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.confirmationCode = calculateConfirmationCode();
+        counter ++;
+    }
+
+    /* #4
+     * getter for the contact information:
+     * returns a string containing the name, phone number and confirmation code.
+     *
+     * accessible in other class
+     */
+    public String getContactInfo() {
+        return "The following are the information of the current contact: \n" +
+                "Name: " + name +
+                ",\nPhone Number: " + this.phoneNumberToString1() +
+                ",\nConfirm Code: " + confirmationCode + ";";
+//        return "The following are the information of the current contact: \n" +
+//                "Name: " + name +
+//                ",\nPhone Number: " + phoneNumberToString2(phoneNumber) +
+//                ",\nConfirm Code: " + confirmationCode + ";";
+    }
+
+    /* #5
+     * setter for the contact information:
+     * reset the name and phone number according to the parameters,
+     * and replace the confirmation code by a newly calculated one.
+     */
+    public void setContactInfo(String name, int[] phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.confirmationCode = calculateConfirmationCode();
+    }
+
+    /* #7
+     * static function to print a welcome message
+     *
+     * accessible in other class
+     */
     public static void welcome() {
         System.out.println("Welcome to the Contact Manager!");
     }
 
+    /* #8
+     * getter for the static counter
+     *
+     * accessible in other class
+     */
     public static int getCounter() {
         return counter;
     }
--- src/Contact_Client.java
+++ src/Contact_Client.java
@@ -1,16 +1,45 @@
 public class Contact_Client {
     public static void main(String[] args) {
-        Contact.welcome();
+        /* confirmation code 在#2加入 （private）
+         * counter在#7加入 （staic）
+         */
 
-        Contact c1 = new Contact("Paul", new int[]{1, 2, 3, 4, 5, 6});
+        // initialization
+        int[] pn1 = {1, 2, 3, 4, 5, 6};
+        int[] pn2 = {6, 5, 4, 3, 2, 1};
+//        int[] pn_temp = {1, 0, 0, 8, 6};
+
+        // #1
+        Contact c1 = new Contact("Paul", pn1);
+        Contact c2 = new Contact("Sean", pn2);
+        System.out.println(c1.name);
+        for (int i = 0; i < c1.phoneNumber.length; i++) {
+            System.out.print(c1.phoneNumber[i]);
+        }
+        System.out.println();
+
+        // #2
+//        System.out.println(", " + c1.confirmationCode);
+//        int cc = c1.calculateConfirmationCode();
+
+        // #3
+//        String pn_temp_str = pn_temp.phoneNumberToString1();
+
+        // #4
         System.out.println(c1.getContactInfo());
 
-        Contact c2 = new Contact("Sean", new int[]{6, 5, 4, 3, 2, 1});
-        System.out.println(c2.getContactInfo());
+        // #5
+        int[] pn2_ = {1, 0, 0, 8, 6};
+        c2.setContactInfo("Henry", pn2_);
 
-        c2.setContactInfo("Henry", new int[]{1, 0, 0, 8, 6});
-        System.out.println(c2.getContactInfo());
 
+        // #6 static
+        Contact.welcome();
+
+        // #7
         System.out.println("Total number of contacts: " + Contact.getCounter());
+
+        // #8
+//        String pn_str = phoneNumberToString2(pn_temp);
     }
 }
